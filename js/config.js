4bfca7cbf4709865660d5546fd271232c06ff0af
16cameronk <Cameron@Camerons-MBP.wired.duke.local>
2017-08-08 17:47:29 -0400

    hi

---
+++ js/config.js
@@ -0,0 +1,26 @@
+
+/* CONFIG                                                                                                         */
+/******************************************************************************************************************/
+
+/* Edit the array as you wish. It will contain the typed strings. */
+var config_typedArray = ["I am an educator.", "I am creative.", "I am an educator.", "I am an academic.", "I am innovative.", "I am a researcher." ];
+
+/* Enable the Typed effect */
+var config_enableTyped = false;
+
+/* Disable Parallax effect on mobile/touch devices for performance reasons. */
+var config_disableParalaxOnTouchDevices = true;
+
+/* Front page effect - can be 'none', 'particles', 'video'  */
+var config_frontPageEffect = 'particles';
+
+/* If config_frontPageEffect is set to video, here set the video URL, type and opacity */
+var config_videoUrl = 'video/background.mp4';
+var config_videoType = 'video/mp4';
+var config_videoOpacity = 0.75;
+
+/* Google map parameters */
+var config_mapLat = 51.509036; // latitude
+var config_mapLng = -0.123280; // longitude
+var config_mapZoom = 16; // map zoom
+var config_mapTitle = 'My company' // marker title
